1332e100347f05f3d0d85c9e838503e8be668356
QQtun <deng.chengdun@gmail.com>
2022-01-11 15:51:23 +0800

    多一個key field

--- Assets/Scripts/Core/Framework/Event/StringKeyEvent.cs
+++ Assets/Scripts/Core/Framework/Event/StringKeyEvent.cs
@@ -3,10 +3,12 @@
     public class StringKeyEvent : EventBase<StringKeyEvent>
     {
         public object Data { get; private set; }
+        public string Key { get; private set; }
 
         public static StringKeyEvent Allocate(string key, object data)
         {
             var evt = Allocate();
+            evt.Key = key;
             evt.EventTag = key;
             evt.Data = data;
             return evt;
